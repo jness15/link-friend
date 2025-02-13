1644534d378d9a5a0f9c1ad6f57ba0ff20742c24
jroxy <simjiyong92@gmail.com>
2025-02-13 09:25:00 +0900

    First commit

---
+++ src/main/kotlin/com/jroxy/linkfriend/LinkFriendApplication.kt
@@ -0,0 +1,11 @@
+package com.jroxy.linkfriend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LinkFriendApplication
+
+fun main(args: Array<String>) {
+	runApplication<LinkFriendApplication>(*args)
+}
---
+++ src/test/kotlin/com/jroxy/linkfriend/LinkFriendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jroxy.linkfriend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LinkFriendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
